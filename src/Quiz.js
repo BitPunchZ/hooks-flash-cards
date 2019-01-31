94065b9f8da72787f8afe3241098579ecec9fee3
BitPunchZ <mpayob2000@gmail.com>
2019-01-31 18:50:22 +0200

    setting up the boilerplate

---
+++ src/Quiz.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Quiz() {
+  return <div className="slider-container">slider container</div>;
+}
+
+export default Quiz;
---
+++ src/QuizQuestionForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+function QuizQuestionForm() {
+  return <div>Quiz Question Form</div>;
+}
+
+export default QuizQuestionForm;
